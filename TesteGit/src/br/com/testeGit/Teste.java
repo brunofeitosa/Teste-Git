4d2fb40eb864cbc33f500dbb27f12c7ecc36877e
bruno.feitosa <bfeitosa@indracompany.com>
2012-04-13 14:10:45 -0300

    teste branch

--- TesteGit/src/br/com/testeGit/Teste.java
+++ TesteGit/src/br/com/testeGit/Teste.java
@@ -4,5 +4,13 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		System.out.println("x");
+		compareStringIgnoreCase("a","A");
+	}
+	
+	private static boolean compareStringIgnoreCase(String a, String b){
+		if (a.equalsIgnoreCase(b)){
+			return true;
+		}
+		return false;
 	}
 }
